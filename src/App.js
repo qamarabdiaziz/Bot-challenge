6c5424071d5a9d266cb44733e92cbe8be689994c
qamarabdiaziz <tushez98@gmail.com>
2023-05-01 20:03:36 +0300

    Add new files and remove deleted files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
- import React from "react"
-import BotsPage from "./containers/BotsPage"
-import "./App.css"
-export default function App() {
-  return (
-    <div className="App">
-      <BotsPage />
-    </div>
-  )
-}
+ import BotsContainer from "./BotsContainer";
+ import "./App.css";
+
+ function App() {
+   return (
+     <div className="App">
+       <BotsContainer />
+     </div>
+   );
+ }
+
+ export default App;
